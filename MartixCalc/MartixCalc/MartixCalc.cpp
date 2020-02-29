e0db1f079853649f2e7a5eb580c168645ca97a36
legGalaxyCode <alexey.zhavoronkov2001@gmail.com>
2020-02-29 15:16:05 +0300

    MatrixCalc v0.2

--- MartixCalc/MartixCalc/MartixCalc.cpp
+++ MartixCalc/MartixCalc/MartixCalc.cpp
@@ -3,55 +3,29 @@
 #include<time.h>
 using namespace std;
 
-class Matrix {
-private:
-	int a11;
-	int a12;
-	int a21;
-	int a22;
-public:
-	Matrix() : a11(0), a12(0), a21(0), a22(0) {};
-	Matrix(int a, int b, int c, int d) : a11(a), a12(b), a21(c), a22(d)
-	{ };
-	void get_elem()
-	{
-		cout << a11 << "	" << a12 << endl << a21 << "	" << a22 << endl;
-	}
-	int get_det()
-	{
-		return (a11 * a22 - a12 * a21);
-	}
-	Matrix operator+(Matrix b)
-	{
-		Matrix temp;
-		temp.a11 = a11 + b.a11;
-		temp.a12 = a12 + b.a12;
-		temp.a21 = a21 + b.a21;
-		temp.a22 = a22 + b.a22;
-		return temp;
-	}
-	Matrix operator*(Matrix b)
-	{
-		Matrix temp;
-		temp.a11 = a11 * b.a11 + a12 * b.a21;
-		temp.a12 = a11 * b.a12 + a12 * b.a22;
-		temp.a21 = a21 * b.a11 + a22 * b.a21;
-		temp.a22 = a21 * b.a12 + a22 * b.a22;
-		return temp;
-	}
-};
-
-//найти определитель
-//перемножить матрицы
+//find determinant - next
+//matrix multiplication - kk
+//left side *num
+//overload *=
+//overload -
+//cin space change
+//functions preload
 class aMatrix {
-private:
-	string err_type = "матрицы не могут быть сложены";
-	string err_type1 = "матрицы не могут быть перемножены";
-	static const int SIZE_L = 3;
-	static const int SIZE_C = 3;
-	int arr[SIZE_L][SIZE_C];
+private: 
+	string err_type = "Matrices can't be stacked";
+	string err_type1 = "Matrices can't be multiplicate";
+	int SIZE_L;
+	int SIZE_C;
+	int** arr;
 public:
-	aMatrix() {
+	aMatrix(unsigned int n, unsigned int m) {
+		SIZE_L = n;
+		SIZE_C = m;
+		arr = new int* [n];
+		for (int i = 0; i < m; i++)
+		{
+			arr[i] = new int[m];
+		}
 		for (int d = 0, c = 0, c1 = 0; d < SIZE_C * SIZE_L; c++, d++) {
 			if (c >= SIZE_L) { c = 0; c1++; };
 			arr[c1][c] = 0;
@@ -67,12 +41,13 @@ public:
 	int get_det()const
 	{
 		int temp;
+		const double EPS = 1E-9;
 		(SIZE_C == SIZE_L) ? (temp = 1) : (temp = 0);
 		if (temp == 0) {
 			return 0;
 		}
 		else {
-
+			return 0; //find det next
 		}
 	}
 	void set_elem()
@@ -95,7 +70,7 @@ public:
 	}
 	aMatrix operator+ (aMatrix buf)
 	{
-		aMatrix temp;
+		aMatrix temp(SIZE_L, SIZE_C);
 		if (SIZE_C == buf.SIZE_C && SIZE_L == buf.SIZE_L)
 		{
 			for (int d = 0, c = 0, c1 = 0; d < SIZE_C * SIZE_L; c++, d++) {
@@ -106,12 +81,12 @@ public:
 		}
 		else {
 			cout << temp.err_type;
-			return temp;
+			return temp; //empty matrix
 		}
 	}
-	aMatrix operator* (int alp)
+	aMatrix operator* (int alp) //fix only left side num
 	{
-		aMatrix temp;
+		aMatrix temp(SIZE_L, SIZE_C);
 		for (int d = 0, c = 0, c1 = 0; d < SIZE_C * SIZE_L; c++, d++) {
 			if (c >= SIZE_L) { c = 0; c1++; };
 			temp.arr[c1][c] = arr[c1][c] * alp;
@@ -120,7 +95,7 @@ public:
 	}
 	aMatrix operator* (aMatrix buf)
 	{
-		aMatrix temp;
+		aMatrix temp(SIZE_L, SIZE_C);
 		if (SIZE_C == buf.SIZE_L)
 		{
 			static int sum;
@@ -140,15 +115,14 @@ public:
 		}
 		else {
 			cout << temp.err_type1;
-			return temp;
+			return temp; //empty matrix
 		}
 	}
 };
 
 int main() {
 	srand(time(NULL));
-	Matrix A(12, 4, 8, 2), B(2, 3, 5, 2);
-	aMatrix C, D, G, T;
+	aMatrix C(2, 2), D(2, 2), G(2, 2), T(2, 2);
 	C.set_random_elem();
 	C.get_elem();
 	cout << endl << endl;
